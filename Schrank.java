b1a31e51e8c9ab2c89cb406f87f3e7d13379475f
RabinRavikumar <rabin.ravikumar@outlook.de>
2026-01-19 16:35:28 +0100

    Update Schrank.java

--- Schrank.java
+++ Schrank.java
@@ -1,31 +1,23 @@
 package Artikelhierarchie;
-public class Schrank extends Moebel {
-
+public class Schrank extends Moebel{
   private int anzahlSchubladen;
   
   public Schrank (int anzahlSchubladen, String material, int artikelnummer,double preis,String produktname){
     super(material,artikelnummer,preis,produktname);
     this.anzahlSchubladen = anzahlSchubladen;
-  
     }
-    
-  public int getAnzahlSchubladen() {
+  public int getAnzahlSchubladen(){
     return anzahlSchubladen;
-  }
-  
-  public void setAnzahlSchubladen(int anzahlSchubladen) {
+    }
+  public void setAnzahlSchubladen(int anzahlSchubladen){
     this.anzahlSchubladen = anzahlSchubladen;
-  }
-
+    }
   public void gibAus(){
     super.gibAus();
     System.out.println("Anzahl Schubladen:" + anzahlSchubladen);
-  
     }
-  
-  public static void main(String[]arg) {
+  public static void main(String[]arg){
     Schrank test1 = new Schrank(10,"Holz",12345, 150.30,"Super3xl");
     test1.gibAus();
-  }  
-  
-  }
+    }  
+    }
