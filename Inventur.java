061687a52beb8aca62ab160aa3cae56ce73311f8
RabinRavikumar <rabin.ravikumar@outlook.de>
2026-01-19 17:25:01 +0100

    Update Inventur.java

--- Inventur.java
+++ Inventur.java
@@ -1,15 +1,22 @@
 package Artikelhierarchie;
+/**
+Verwaltet ein Sortiment aus Artikeln und ermöglicht das Ausgeben
+und Ersetzen einzelner Elemente.
+**/
 public class Inventur{
   private Artikel[] sortiment;
   
   public Inventur(Artikel[] sortiment){
     this.sortiment = sortiment;
     }
+
+  //Gibt alle Artikel im Sortiment aus.
   public void gibAus(){
     for (int i = 0; i < sortiment.length; i++){
       sortiment[i].gibAus();
       } // end of for  
     }
+  //Gibt einen einzelnen Artikel an der angegebenen Position aus.
   public void gibAus(int zahl){
     sortiment[zahl].gibAus();
     }
@@ -19,18 +26,4 @@ public class Inventur{
   public Artikel getArtikel(int zahl){
     return sortiment[zahl];
     }
-  public static void main(String[]args){
-    Fernseher Phillips = new Fernseher(47,true, 300,1457,"Phillips");
-    DigitalKamera Canon=new DigitalKamera(12.1,3,14.99,56780,"ZoomMaster 3x");
-    DVDplayer test1 = new DVDplayer(4160,12.00, "Player2800", "HDMI", true);
-    Artikel[] sortiment = new Artikel[3];
-    sortiment[0] = Phillips;
-    sortiment[1] = Canon;
-    sortiment[2] = test1;
-    Inventur test = new Inventur(sortiment);
-    test.gibAus();
-    test.gibAus(1);
-    System.out.println(test.getArtikel(2));  
-  }
-  }
-
+    }
