343686d2758808131cf518bf30898e9d450535a1
RabinRavikumar <rabin.ravikumar@outlook.de>
2026-01-19 16:36:31 +0100

    Update Sofa.java

--- Sofa.java
+++ Sofa.java
@@ -18,14 +18,9 @@ public class Sofa extends Moebel{
   public static void main (String[]args){
     Sofa s1 = new Sofa (3, "3 Minecraft Wolle, 3 Holckblöcke von Badlandmanssion",017622547,1999.99,"Minecraft Kuschelstuhl-2000-New/Gen");
     s1.gibAus();
-    
     System.out.println("Die anzahl derSitzgelegenheiten beträgt:");
     s1.getAnzahlSitzGelegenheiten();
     System.out.println(s1.getAnzahlSitzGelegenheiten());
     s1.getAnzahlSitzGelegenheiten();
-    
-    
-    
-    
     }
-  }
+    }
