2e26fb0ecee67b0146421d6736a57ec16b5c0f71
RabinRavikumar <rabin.ravikumar@outlook.de>
2026-01-19 16:32:38 +0100

    Update Inventur.java

--- Inventur.java
+++ Inventur.java
@@ -6,7 +6,7 @@ public class Inventur{
     this.sortiment = sortiment;
     }
   public void gibAus(){
-    for (int i = 0; i < sortiment.length; i++) {
+    for (int i = 0; i < sortiment.length; i++){
       sortiment[i].gibAus();
     } // end of for  
       }
@@ -20,7 +20,7 @@ public class Inventur{
     return sortiment[zahl];
     }
     
-  public static void main(String[]args) {
+  public static void main(String[]args){
     Fernseher Phillips = new Fernseher(47,true, 300,1457,"Phillips");
     DigitalKamera Canon=new DigitalKamera(12.1,3,14.99,56780,"ZoomMaster 3x");
     DVDplayer test1 = new DVDplayer(4160,12.00, "Player2800", "HDMI", true);
@@ -31,7 +31,6 @@ public class Inventur{
     Inventur test = new Inventur(sortiment);
     test.gibAus();
     test.gibAus(1);
-    System.out.println(test.getArtikel(2));
-    
+    System.out.println(test.getArtikel(2));  
   }
   }
