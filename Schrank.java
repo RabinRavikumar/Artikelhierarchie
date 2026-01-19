83c89b5fcb85c425efaf21b384fdcdafb9c3e5eb
RabinRavikumar <rabin.ravikumar@outlook.de>
2026-01-19 17:49:14 +0100

    Update Schrank.java

--- Schrank.java
+++ Schrank.java
@@ -1,4 +1,8 @@
 package Artikelhierarchie;
+/**
+Spezialisierte Unterklasse von Möbel.
+Repräsentiert ein Schrank mit einer Anzahl an Schubladen.
+**/
 public class Schrank extends Moebel{
   private int anzahlSchubladen;
   
@@ -15,9 +19,5 @@ public class Schrank extends Moebel{
   public void gibAus(){
     super.gibAus();
     System.out.println("Anzahl Schubladen:" + anzahlSchubladen);
-    }
-  public static void main(String[]arg){
-    Schrank test1 = new Schrank(10,"Holz",12345, 150.30,"Super3xl");
-    test1.gibAus();
     }  
     }
