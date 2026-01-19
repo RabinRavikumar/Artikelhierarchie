b9fc0dba72f7136254f76af8ac775c1e6059047d
RabinRavikumar <rabin.ravikumar@outlook.de>
2026-01-19 16:34:03 +0100

    Update Moebel.java

--- Moebel.java
+++ Moebel.java
@@ -1,34 +1,24 @@
 package Artikelhierarchie;
-public class Moebel extends Artikel
-{
+public class Moebel extends Artikel{
   private String material;
   
-  public Moebel (String material, int artikelnummer, double preis,  String produktname)
-  {
+  public Moebel (String material, int artikelnummer, double preis,  String produktname){
     super(artikelnummer,preis,produktname);
     this.material= material;
     }
-
-  public String getMaterial()
-  {
+  public String getMaterial(){
     return material;
     }
-
-  public void  setMaterial (String material)
-  {
+  public void  setMaterial (String material){
     this.material= material;
     }
-    
-  public void gibAus()
-  {
+  public void gibAus(){
     System.out.println("Das Möbelstück besteht aus "+material);
     System.out.println("-----------------------------------");
     super.gibAus();
       }  
-   
-  public static void main(String[]arg) {
+  public static void main(String[]arg){
     Moebel Joop = new Moebel("Holz", 25334, 1000, "Schreibtisch");
-    
     Joop.gibAus();
   }
   }
